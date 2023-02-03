6d660ab648de86549c2f1b61b390454e2f64d03b
justinsospeter <justinsospeter@gmail.com>
2023-02-03 22:42:27 +0300

    adding content

--- client/src/Components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/Components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,44 +1,13 @@
 import React from "react";
 import Card from "../Card/Card";
 import "./FeaturedProducts.scss";
+import useFetch from "../../hooks/useFetch";
 
-function FeaturedProducts({ type }) {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/794062/pexels-photo-794062.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/794063/pexels-photo-794063.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Long sleeve grafic shirt",
-      isNew: true,
-      oldPrice: 19,
-      newPrice: 12,
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/914668/pexels-photo-914668.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Coat",
-      isNew: false,
-      oldPrice: 19,
-      newPrice: 12,
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/4937449/pexels-photo-4937449.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/12872209/pexels-photo-12872209.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Shirt",
-      isNew: true,
-      oldPrice: 19,
-      newPrice: 12,
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/41527/beautiful-brown-country-cute-41527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Hat",
-      isNew: false,
-      oldPrice: 19,
-      newPrice: 12,
-    },
-  ];
+const FeaturedProducts = ({ type }) => {
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=${type}`
+  );
+
   return (
     <div className="featuredproducts">
       <div className="top">
@@ -52,12 +21,14 @@ function FeaturedProducts({ type }) {
         </p>
       </div>
       <div className="bottom">
-        {data.map((item) => (
-          <Card item={item} key={item.id} />
-        ))}
+        {error
+          ? "Something went wrong!"
+          : loading
+          ? "loading"
+          : data?.map((item) => <Card item={item} key={item.id} />)}
       </div>
     </div>
   );
-}
+};
 
 export default FeaturedProducts;
